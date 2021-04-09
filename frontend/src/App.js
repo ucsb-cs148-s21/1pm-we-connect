22c6456d8c352153681ed4eb787d87f8487319e7
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-04-09 13:24:40 -0700

    Add Hello World application

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h>
+      Welcome to the We Connect App.
+        </h>
+      </header>
+    </div>
+  );
+}
+
+export default App;
