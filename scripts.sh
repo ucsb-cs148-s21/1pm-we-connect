84aeb0f4b773bca2b96fdb83324555836175884d
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-04-15 14:04:46 -0700

    Add more options to scripts

--- scripts.sh
+++ scripts.sh
@@ -5,10 +5,15 @@ case $1 in
         then
             echo "Poetry could not be found, please install it: https://python-poetry.org/docs/"
         exit
+        elif ! command -v npm &> /dev/null
+             then
+                echo "npm/Node could not be found, please install it: https://nodejs.dev/learn/how-to-install-nodejs"
         else
             poetry install
+            (cd ./frontend/; npm install)
         fi;;
-    serve ) python3 wsgi.py;;
+    serve ) python3 ./wsgi.py;;
+    frontend-serve ) (cd ./frontend/; npm start);;
     frontend-build ) (cd ./frontend/; npm run build; cp -r build/ ../src/);;
-    *) echo "Please enter a command of: install, serve or frontend-build";;
+    *) echo "Please enter a command of: install(installs the required dependencies), serve(serve the backend flask app), frontend-serve(serves the frontend react) or frontend-build(builds the frontend for production)";;
 esac
