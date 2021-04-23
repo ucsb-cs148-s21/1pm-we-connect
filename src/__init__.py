ee43d77a789946f2e425ea060549db5eda4fa07e
Kevin H <khoang@ucsb.edu>
2021-04-22 17:44:56 -0700

    GET, POST, UPDATE, and DELETE basic implementation added; Tested only on localhost through spike for now

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,9 @@
 from flask import Flask, render_template,url_for
 # from flask_sqlalchemy import SQLAlchemy
 #
+
+# Temporary array before implementing database
+storage = ["Item 1", "Item 2", "Item 3"]
 def create_app():
     # app = Flask(__name__, static_folder="./build/static", template_folder="./build") # For React to be added later
     app = Flask(__name__, static_folder="./templates/static", template_folder="./templates")
@@ -9,10 +12,32 @@ def create_app():
         @app.route("/")
         def index():
             return render_template("index.html") # Landing page, maybe also contain projects but optional
+        
         @app.route("/projects")
         def projects():
-            return "projects"
-        @app.route("/projects/<int:post_id>") # There's a special "converter" with flask, so you don't have to convert to int anymore
-        def project(post_id):
-            return "project " + post_id
+            return ", ".join(storage) 
+
+        #Get, Post, Update, or Delete
+        @app.route("/projects/edit", methods=["GET", "POST", "UPDATE", "DELETE"])
+        def projectCreate():
+            if(request.method == "GET"):
+                index = request.form["index"]
+                return storage[int(index)]
+
+            elif(request.method == "POST"):
+                value = request.form["value"]
+                storage.append(value)
+                return value
+
+            elif(request.method == "UPDATE"):
+                index = request.form["index"]
+                value = request.form["value"]
+                storage[int(index)] = value
+                return storage[int(index)]
+
+            elif(request.method == "DELETE"):
+                index = request.form["index"]
+                storage.pop(int(index))
+                return index
+
         return app
