da073f866e77c668283fe23bc50273978bd23c6f
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-04-26 07:33:53 -0700

    Remove the use of the styled components lib, make minor changes to navbar, and remove unncessary package.json

--- frontend/src/components/NavBar/NavBarElements.js
+++ frontend/src/components/NavBar/NavBarElements.js
@@ -1,86 +1,24 @@
-import { FaBars } from 'react-icons/fa';
+// import { FaBars } from 'react-icons/fa';
 import { NavLink as Link } from 'react-router-dom';
-import styled from 'styled-components';
+import "./NavBarElements.css";
 
-export const Nav = styled.nav`
-  background: #000;
-  height: 80px;
-  display: flex;
-  justify-content: space-between;
-  padding: 0.5rem calc((100vw - 1000px) / 2);
-  z-index: 10;
-  /* Third Nav */
-  /* justify-content: flex-start; */
-`;
+const myStyled = (className, Component) => {
+  return ({ children, ...props }) => ( <Component className={className} {...props}> { children } </Component> )
+}
+export const Nav = ({ children }) => (
+  <nav className="navbar">
+    { children}
+  </nav>
+)
 
-export const NavLink = styled(Link)`
-  color: #fff;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  padding: 0 1rem;
-  height: 100%;
-  cursor: pointer;
-  &.active {
-    color: #15cdfc;
-  }
-`;
+export const NavLink = ({ children, ...props }) => (
+  <Link className="navbar-link" {...props}>
+    { children }
+  </Link>
+)
 
-export const Bars = styled(FaBars)`
-  display: none;
-  color: #fff;
-  @media screen and (max-width: 768px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 75%);
-    font-size: 1.8rem;
-    cursor: pointer;
-  }
-`;
-
-export const NavMenu = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-  white-space: nowrap; */
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
-
-export const NavBtn = styled.nav`
-  display: flex;
-  align-items: center;
-  margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
-
-export const NavBtnLink = styled(Link)`
-  border-radius: 4px;
-  background: #256ce1;
-  padding: 10px 22px;
-  color: #fff;
-  outline: none;
-  border: none;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  text-decoration: none;
-  /* Second Nav */
-  margin-left: 24px;
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #010606;
-  }
-`;
+const div = ({ children, ...props}) => (<div {...props} >{children}</div>)
+export const NavMenu = myStyled("nav-menu", div)
+const nav = ({ children, ...props }) => (<nav {...props}>{children}</nav>)
+export const NavBtn = myStyled("nav-button", nav)
+export const NavBtnLink = myStyled("nav-button-link", Link)
