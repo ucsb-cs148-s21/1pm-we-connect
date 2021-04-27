51a810e49e14f8b22816f7c03c4ad2ccb0a0708f
Surendra <surendra0927@gmail.com>
2021-04-27 16:30:05 -0700

    can fetch

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
+	const [data, setData] = useState([])
+
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+
+  	const getData = async () =>{
+  		fetch('https://jsonplaceholder.typicode.com/todos/1')
+  		.then(response => response.json())
+  		.then(json => setData(json))
+  	}
+
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
             <h1>Home</h1>
+            <h1>{data.title}</h1>
         </div>
     )
 }
