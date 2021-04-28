0d9cc17dc7b5ced22b7d6fd1d402cae8716c6552
Kevin H <khoang@ucsb.edu>
2021-04-27 23:46:20 -0700

    Added database, has a table not found error I dunno how to fix, DOES NOT WORK

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python3
 from flask import Flask, request, render_template, url_for, Response
-#from flask_sqlalchemy import SQLAlchemy
-
-#db = SQLAlchemy()
-# Temporary dictionary of dictionaries
-storage = {
-    "1": {"author": "Author 1", "projectName":"Make a Flask app", "contactInfo": "111-111-1111", "projectDescription": "hello"},
-    "2": {"author": "Author 2", "projectName":"Stop being depressed", "contactInfo": "111-111-1111", "projectDescription": "hello"},
-    "3": {"author": "Author 3", "projectName":"Eat", "contactInfo": "111-111-1111", "projectDescription": "hello"}
-    }
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+import os.path
+from os import path
+import random
 
 def create_app():
     # app = Flask(__name__, static_folder="./build/static", template_folder="./build") # For React to be added later
     app = Flask(__name__, static_folder="./templates/static", template_folder="./templates")
+
+    #database configuration
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    db = SQLAlchemy(app)
+
+    #Removed for database testing
+    """ 
+    # Temporary dictionary of dictionaries
+    storage = {
+        "1": {"author": "Author 1", "projectName":"Make a Flask app", "contactInfo": "111-111-1111", "projectDescription": "hello"},
+        "2": {"author": "Author 2", "projectName":"Stop being depressed", "contactInfo": "111-111-1111", "projectDescription": "hello"},
+        "3": {"author": "Author 3", "projectName":"Eat", "contactInfo": "111-111-1111", "projectDescription": "hello"}
+        }
+    """
+
+    class formModel(db.Model):
+        __tablename__ = 'form'
+
+        id = db.Column(db.Integer, primary_key = True)
+        author = db.Column(db.String(100), nullable = False)
+        projectName = db.Column(db.String(100), nullable = False)
+        contactInfo = db.Column(db.String(12), nullable = False)
+        projectDescription = db.Column(db.String(2000), nullable = False)
+
+        def __repr__(self):
+            item = {
+                "author": author,
+                "projectName": projectName,
+                "contactInfo": contactInfo,
+                "projectDescription": projectDescription
+            }
+            return item
+
     with app.app_context():
         @app.route("/")
         def index():
@@ -22,28 +51,54 @@ def create_app():
         @app.route("/projects", methods=["GET", "POST"])
         def projects():
             if(request.method == "GET"):
+                allForms = formModel.query.all()
+                storage = {}
+                for form in allForms:
+                    storage[str(form.id)] = {
+                        "author": form.author, 
+                        "projectName": form.projectName, 
+                        "contactInfo": form.contactInfo, 
+                        "projectDescription": form.projectDescription
+                    }
                 return storage
 
             elif(request.method == "POST"):
-                author = request.form["author"]
-                projectName = request.form["projectName"]
-                contactInfo = request.form["contactInfo"]
-                projectDescription = request.form["projectDescription"]
+                #Removed for database testing
+                
+                authorArg = request.form["author"]
+                projectNameArg = request.form["projectName"]
+                contactInfoArg = request.form["contactInfo"]
+                projectDescriptionArg = request.form["projectDescription"]
+                """
                 item = {
                     "author": author,
                     "projectName": projectName,
                     "contactInfo": contactInfo,
                     "projectDescription": projectDescription
                 }
-                number = str(id(item))
-                storage[number] = item
-                return storage[number]
+                """
+                #Taken out for database testing
+                #number = id(item) 
+                number = random.randrange(1,100)
+
+                form = formModel(
+                    id = number, 
+                    author = authorArg, 
+                    projectName = projectNameArg,
+                    contactInfo = contactInfoArg,
+                    projectDescription = projectDescriptionArg
+                    )
+                db.session.add(form)
+                db.session.commit()
+
+                return "SUCCESS"
 
         # Get, Put, or Delete
-        @app.route("/projects/<id>", methods=["GET", "PUT", "DELETE"])
-        def projectCreate(id):
+        @app.route("/projects/<int:form_id>", methods=["GET", "PUT", "DELETE"])
+        def projectCreate(form_id):
             if(request.method == "GET"):
-                return storage[id]
+                result = formModel.query.get(id=form_id)
+                return result
 
             # elif(request.method == "PUT"):
             #     storage[id] = {"name": id, "text": value}
