264c763a3f0026408f38b5a8b58530bf7e499454
Alon Katz <alonkatz@ucsb.edu>
2021-05-06 16:05:23 -0700

    Changed the create post to Material Ui components and made it that the page routes to the home page once form is submitted

--- frontend/src/components/PostForm/index.jsx
+++ frontend/src/components/PostForm/index.jsx
@@ -1,67 +1,90 @@
 import React from 'react'
+import { TextField, Grid, Button } from '@material-ui/core';
 const PostForm = () => {
-    const [name, setName] = React.useState('')
-    const [projectName, setProjectName] = React.useState('')
-    const [text, setText] = React.useState('')
-    const [contact, setContact] = React.useState('')
+    const [name, setName] = React.useState("")
+    const [projectName, setProjectName] = React.useState("")
+    const [text, setText] = React.useState("")
+    const [contact, setContact] = React.useState("")
     //const [topic, setTopic] = React.useState('cs')
+
 
     const submitForm = (e) => {
     	e.preventDefault();
+        var submit = true;
+        
+        if (name.localeCompare("") === 0){
+            submit = false;
+            alert("Must fill in the name box");
+        }
 
+        if (projectName.localeCompare("") === 0){
+            submit = false;
+            alert("Must fill in the project name box");
+        }
+
+        if (contact.localeCompare("") === 0){
+            submit = false;
+            alert("Must fill in the contact box");
+        }
+
+        if (text.localeCompare("") === 0){
+            submit = false;
+            alert("Must fill in the project description box");
+        }
     	//from reactDocs
-    	fetch('/projects', {
-  			method: 'POST',
-  			headers: {
-    			Accept: 'application/json',
-    			'Content-Type': 'application/json'
-  			},
-  			body: JSON.stringify({
-    			author: name,
-    			projectName: projectName,
-    			contactInfo: contact,
-    			projectDescription: text
 
-  			})
-		});
+        if (submit === true){
+            fetch('/projects', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    author: name,
+                    projectName: projectName,
+                    contactInfo: contact,
+                    projectDescription: text
 
-		alert("Form submitted")
+                })
+            });
+
+            alert("Form submitted")
+            window.location.replace("/")
+        }
+
+		
     }
 
     return (
         <form  style={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
-            <div>
-                <p>Name:</p>
-                <input onChange={e => setName(e.target.value)} value={name} name="author"/>
-            </div>
+            <Grid container direction={"column"} spacing={2}>
+                <Grid item>
+                    <h2 >
+                        Create a Post
+                    </h2>
+                </Grid>
+                <Grid item>
+                    <TextField id="standard-basic" label="Name" onChange={e => setName(e.target.value)} value={name} name="author" variant="outlined"/>
+                </Grid>
+                <Grid item>
+                    <TextField id="filled-basic" label="Project Name" variant="outlined"  onChange={e => setProjectName(e.target.value)} paddingTop={2}/>
+                </Grid>
+                <Grid item>
+                    <TextField id="outlined-basic" label="Contact" variant="outlined" onChange={e => setContact(e.target.value)} paddingTop={2}/>
+                </Grid>
+                <Grid item>
+                    <TextField id="outlined-basic" label="Project Description" variant="outlined" onChange={e => setText(e.target.value)} multiline paddingTop={2}/>
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" color="primary" onClick={submitForm}>
+                        Submit
+                    </Button>
+                </Grid>
+            </Grid>
 
-            <div>
-                <p>Project Name:</p>
-                <input onChange={e => setProjectName(e.target.value)} value={projectName} name="projectName"/>
-            </div>
-
-            <div>
-                <p>Contact:</p>
-                <input onChange={e => setContact(e.target.value)} value={contact} name="contactInfo"/>
-            </div>
-
-          <div>
-                <p>Project Description:</p>
-                <textarea type='text' value={text} onChange={e => setText(e.target.value)} name="projectDescription"/>
-            </div>
-
-
-            {/*<div>
-                <p>Major </p>
-                <select value={topic} onChange={e => setTopic(e.target.value)}>
-                    <option value="cs">Computer Science</option>
-                    <option value="phys">Physics</option>
-                    <option value="film">Film</option>
-                </select>
-            </div>*/}
-            {<button onClick={submitForm} type="submit">Submit</button>}
-           {/* <button type="submit">Submit</button>*/}
         </form>
     )
 }
 export default PostForm
+
