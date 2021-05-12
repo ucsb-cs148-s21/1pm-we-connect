19aaf1908e8830c267ae36672cf351971eec62d4
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-05-12 15:46:40 -0700

    Update config to use the config.py file

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     # app = Flask(__name__, static_folder="./templates/static", template_folder="./templates")
 
     #database configuration
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config.from_object('config.Config')
     db.init_app(app)
 
     if not path.isfile("database.db"):
