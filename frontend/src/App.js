4e172e03404feb662a55782983bb127e3c895e74
Kenneth Deng <kdeng@umail.ucsb.edu>
2021-05-14 14:26:38 -0700

    add new nav bar, about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
+import About from './pages/About'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home}/>
         <Route path="/createPost" exact component={CreatePost}/>
+        <Route path="/about" exact component={About}/>
       </Switch>
     </Router>
   );
