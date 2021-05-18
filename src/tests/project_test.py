6d65ffa13d4f10c6dbee8b179c43aab53b2ee39c
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-05-18 15:28:51 -0700

    Reinitialize the database every time a new test runs

--- src/tests/project_test.py
+++ src/tests/project_test.py
@@ -1,15 +1,7 @@
-from ..__init__ import create_app
 from ..models import db, formModel, tagModel
 from flask import url_for
-import pytest
 import json
 
-# uses the https://pytest-flask.readthedocs.io/en/latest/features.html for pytest-fixtures
-@pytest.fixture
-def app():
-    # make sure the environment variabe TESTING=True
-    app = create_app()
-    return app
 def test_post_request(client):
     form = {"author": "thanawat", "projectName": "hello", "contactInfo": "111-111-1111", "projectDescription": "hello world", "tags": "hello"}
     res = client.post(
