5e66c1d8b1b184a4d7a11eea69732def8cdf6211
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-05-19 21:26:03 -0700

    Empty the database before tests so tests pass

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ def app():
     # make sure the environment variabe TESTING=True
     app = create_app()
     with app.app_context():
+        db.drop_all()
         db.create_all()
     yield app
     with app.app_context():
