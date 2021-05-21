de9f91b0379ad124990f5cc400a02fa9e4346c4a
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-05-21 13:39:48 -0700

    Fix merge conflict and add dark theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,29 @@
-<<<<<<< HEAD
-import './App.css';
+import { useState } from "react"
 import NavBar from './components/NavBar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import About from './pages/About'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from "theme"
+import { CssBaseline, useMediaQuery } from '@material-ui/core';
 
 function App() {
+  const [dark, setDark] = useState(true)
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/createPost" exact component={CreatePost}/>
-        <Route path="/about" exact component={About}/>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme(prefersDarkMode && dark)}>
+      <CssBaseline/>
+      <Router>
+        <NavBar setDark={setDark} dark={dark} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/createPost" exact component={CreatePost} />
+          <Route path="/about" exact component={About} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
-=======
-import NavBar from "./components/NavBar"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import CreatePost from "./pages/CreatePost"
-
-function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/createPost" exact component={CreatePost} />
-            </Switch>
-        </Router>
-    )
->>>>>>> main
 }
 
 export default App
