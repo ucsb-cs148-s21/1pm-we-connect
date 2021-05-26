bb0598498acb0b81230ffceb171289732b1ac74e
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-05-26 15:31:28 -0700

    Add the drawer when on mobile and change dark theme icon

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   AppBar,
   Toolbar,
   Button,
-  Switch,
-  Typography
+  Tooltip,
+  Typography,
+  useMediaQuery,
+  useTheme
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import IconButton from '@material-ui/core/IconButton';
+import Brightness6 from "@material-ui/icons/Brightness6";
+import Brightness7 from "@material-ui/icons/Brightness7";
+import Menu from "@material-ui/icons/Menu";
+import Drawer from "./Drawer"
+
 import { NavLink as Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
@@ -28,24 +36,43 @@ const useStyles = makeStyles((theme) => ({
 
 const Navbar = ({ setDark, dark }) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false)
+  const theme = useTheme()
+  const mobile = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolbar}>
           <Typography className={classes.title} variant="h6" component={Link} to='/'>
-            We Connect
+                We Connect
           </Typography>
-          <div>
-            <Button color="inherit" component={Link} to='createPost'>Create Post</Button>
-            <Button color="inherit" component={Link} to='about'>About</Button>
-            <Button color="inherit" component={Link} to='help'>Help</Button>
-            <Switch
-              checked={dark}
-              onChange={() => setDark(prev => !prev)}
-              name="darktheme"
-            />
-            <Typography variant="h6" component="label"/>
-          </div>
+          {
+            mobile &&
+            <>
+              <Drawer dark={dark} setDark={setDark} open={open} setOpen={() => setOpen(prev => !prev)} />
+              <IconButton onClick={() => setOpen(prev => !prev)}>
+                <Menu />
+              </IconButton>
+            </>
+          }
+
+          {
+            !mobile &&
+            <>
+              <div>
+                <Button color="inherit" component={Link} to='createPost'>Create Post</Button>
+                <Button color="inherit" component={Link} to='about'>About</Button>
+                <Button color="inherit" component={Link} to='/help'>Help</Button>
+                <Tooltip title="Set Dark Theme">
+                  <IconButton onClick={() => setDark(prev => !prev)}>
+                    {dark ? <Brightness6 /> : <Brightness7 />}
+                  </IconButton>
+                </Tooltip>
+                <Typography variant="h6" component="label" />
+              </div>
+            </>
+          }
         </Toolbar>
       </AppBar>
     </div>
