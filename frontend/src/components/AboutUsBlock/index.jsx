01da38411a77e74190b3ee441da3ca55b5f88719
Kenneth Deng <kdeng@umail.ucsb.edu>
2021-05-26 10:13:17 -0700

    added breakpoint for small, adjusts photo size

--- frontend/src/components/AboutUsBlock/index.jsx
+++ frontend/src/components/AboutUsBlock/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
 import { findByLabelText } from '@testing-library/dom';
 import { red } from '@material-ui/core/colors';
-import { Card, CardContent, Typography, Box } from "@material-ui/core"
+import { Grid, Typography, Box, useMediaQuery } from "@material-ui/core"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
-        maxWidth: 1000,
-        margin: "50px 150px 50px 150px",
+        [theme.breakpoints.up('sm')]: {
+            padding: "50px",
+            width: "75%",
+            margin: "50px",
+          },
+          [theme.breakpoints.down('sm')]: {
+            padding: "50px 5px 50px 5px",
+            width: "100%",
+            marginTop: "50px",
+          },
+
+        height: "100%",
+        backgroundColor: "#424242",
     },
-    person: {
+    image: {
         display: "flex",
-        flexDirection: "row",
+        flexDirection: "column",
         alignItems: "center",
     },
 
@@ -19,57 +30,40 @@ const useStyles = makeStyles({
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        margin: "0px 50px 0px 50px",
+        //padding: "50px 0px 50px 0px"
     },
     link: {
         textDecoration: 'underline',
         color: 'inherit',
-    }
-})
+    },
+}))
 
-const AboutUsBlock = ( {imageLink, name, description, direction} ) => {
-    const classes = useStyles()
-    if(direction){
+const AboutUsBlock = ( {imageLink, name, description} ) => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const sm = useMediaQuery(theme.breakpoints.down("sm"));
+
     return (
-        <Card className={classes.root}>
-            <CardContent className={classes.person}>
+        <Grid className={classes.root}
+            container>
+            <Grid className={classes.image} container item> 
                 <img 
                     src={imageLink}
-                    height="400px" width="400px"
+                    height={sm ? "300" : "400"}
+                    width={sm ? "300" : "400"}
+                    loading="lazy"
                 />
-                <div className={classes.text}>
+            </Grid>
+            <Grid className={classes.text} container item>
                     <Typography variant="h4" component="h1" gutterBottom>
                         {name}
                     </Typography>
                     <Typography variant="p" component="p">
                         {description}
                     </Typography>
-                </div>
-            </CardContent>
-        </Card>
+            </Grid>
+        </Grid>
     )
-    }
-    else{
-    return (
-        <Card className={classes.root}>
-            <CardContent className={classes.person}>
-
-                <div className={classes.text}>
-                    <Typography variant="h4" component="h1" gutterBottom>
-                        {name}
-                    </Typography>
-                    <Typography variant="p" component="p">
-                        {description}
-                    </Typography>
-                </div>
-                <img 
-                    src={imageLink}
-                    height="400px" width="400px"
-                />
-            </CardContent>
-        </Card>
-    )
-    }
 }
 
 const AboutUsBlocks = ({ list }) => {
@@ -85,7 +79,18 @@ const AboutUsBlocks = ({ list }) => {
             are my motivators. Click <a className={useStyles().link} href="https://www.pringles.com/us/home.html">here</a> if 
             you want to learn more about me.
             </Typography>,
-            direction: true
+        },
+        {
+            imageLink: "https://wow.zamimg.com/uploads/screenshots/normal/935652.jpg",
+            name: "Mr. Pringles Man", 
+            description: 
+            <Typography>
+            I'm a simple man. My potato chips are crispy, delicious, and quite exquisite. 
+            My eyes may seem lifeless, but I feel great. In fact, I'm a completely changed man.
+            The people who don't believe me are just haters. But you know what they say, haters
+            are my motivators. Click <a className={useStyles().link} href="https://www.pringles.com/us/home.html">here</a> if 
+            you want to learn more about me.
+            </Typography>,
         },
         {
             imageLink: "https://mma.prnewswire.com/media/1346408/Pringles_Logo.jpg?p=facebook",
@@ -98,7 +103,18 @@ const AboutUsBlocks = ({ list }) => {
             are my motivators. Click <a className={useStyles().link} href="https://www.pringles.com/us/home.html">here</a> if 
             you want to learn more about me.
             </Typography>,
-            direction: false
+        },
+        {
+            imageLink: "https://wow.zamimg.com/uploads/screenshots/normal/935652.jpg",
+            name: "Mr. Pringles Man", 
+            description: 
+            <Typography>
+            I'm a simple man. My potato chips are crispy, delicious, and quite exquisite. 
+            My eyes may seem lifeless, but I feel great. In fact, I'm a completely changed man.
+            The people who don't believe me are just haters. But you know what they say, haters
+            are my motivators. Click <a className={useStyles().link} href="https://www.pringles.com/us/home.html">here</a> if 
+            you want to learn more about me.
+            </Typography>,
         },
         {
             imageLink: "https://mma.prnewswire.com/media/1346408/Pringles_Logo.jpg?p=facebook",
@@ -111,41 +127,15 @@ const AboutUsBlocks = ({ list }) => {
             are my motivators. Click <a className={useStyles().link} href="https://www.pringles.com/us/home.html">here</a> if 
             you want to learn more about me.
             </Typography>,
-            direction: true
-        },
-        {
-            imageLink: "https://mma.prnewswire.com/media/1346408/Pringles_Logo.jpg?p=facebook",
-            name: "Mr. Pringles Man", 
-            description: 
-            <Typography>
-            I'm a simple man. My potato chips are crispy, delicious, and quite exquisite. 
-            My eyes may seem lifeless, but I feel great. In fact, I'm a completely changed man.
-            The people who don't believe me are just haters. But you know what they say, haters
-            are my motivators. Click <a className={useStyles().link} href="https://www.pringles.com/us/home.html">here</a> if 
-            you want to learn more about me.
-            </Typography>,
-            direction: false
-        },
-        {
-            imageLink: "https://mma.prnewswire.com/media/1346408/Pringles_Logo.jpg?p=facebook",
-            name: "Mr. Pringles Man", 
-            description: 
-            <Typography>
-            I'm a simple man. My potato chips are crispy, delicious, and quite exquisite. 
-            My eyes may seem lifeless, but I feel great. In fact, I'm a completely changed man.
-            The people who don't believe me are just haters. But you know what they say, haters
-            are my motivators. Click <a className={useStyles().link} href="https://www.pringles.com/us/home.html">here</a> if 
-            you want to learn more about me.
-            </Typography>,
-            direction: true
         },
     ]
+
     return (
         <Box
             display="flex"
             flexDirection="column"
-            justifyContent="space-between"
             alignItems="center"
+        
         >
             {list && list.map((x) => <AboutUsBlock {...x} />)}
         </Box>
